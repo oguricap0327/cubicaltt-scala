def50965e7306ff37628c231f437df6b84469a45
Oguri Cap <oguri.cap@hotmail.com>
2026-03-26 17:50:22 +0800

    parser: add comments, tuples, telescopes, path abs, split
    - Comment support: -- line comments
    - Tuple syntax: (a, b, c)
    - Telescope binders: (x y : A) (z : B) -> C
    - Path abstractions: <i> expr
    - Split expressions: split { pat -> expr }
    - Arrow types: A -> B
    - Function decls with params: f (x : A) : B = body
    
    Still failing all tests - needs more grammar rules

--- src/main/scala/cubical/Parser.scala
+++ src/main/scala/cubical/Parser.scala
@@ -12,7 +12,9 @@ object Parser extends RegexParsers:
   // ── Lexical ──────────────────────────────────────────────
   
   override def skipWhitespace = true
-  override protected val whiteSpace = """(\s|--.*)+""".r  // Handle comments
+  
+  // Skip line comments starting with --
+  protected override val whiteSpace = """(\s|--[^\n]*)+""".r
   
   val reserved = Set("data", "module", "where", "import", "U", "PathP", "comp", "fill", "Glue", "glue", "unglue", "split")
   
@@ -27,16 +29,35 @@ object Parser extends RegexParsers:
   def term: Parser[Term] = expr
   
   def expr: Parser[Term] = 
-    piType | sigmaType | pathType | lamExpr | app
+    piType | sigmaType | pathType | pathAbs | lamExpr | splitExpr | arrowType | app
   
+  // Multi-parameter binders: (x y z : A)
+  def binder: Parser[(List[String], Term)] =
+    ("(" ~> rep1(ident) <~ ":") ~ (term <~ ")") ^^ {
+      case xs ~ ty => (xs, ty)
+    }
+  
+  // Telescope: (x : A) (y : B) -> C
   def piType: Parser[Term] =
-    ("(" ~> ident <~ ":") ~ (term <~ ")") ~ ("->" ~> term) ^^ {
-      case x ~ a ~ b => Pi(a, Lam(x, a, b))
+    rep1(binder) ~ ("->" ~> term) ^^ {
+      case binders ~ body =>
+        binders.foldRight(body) { case ((xs, ty), acc) =>
+          xs.foldRight(acc)((x, b) => Pi(ty, Lam(x, ty, b)))
+        }
+    }
+  
+  // Simple arrow: A -> B (no binder)
+  def arrowType: Parser[Term] =
+    atom ~ ("->" ~> term) ^^ {
+      case a ~ b => Pi(a, Lam("_", a, b))
     }
   
   def sigmaType: Parser[Term] =
-    ("(" ~> ident <~ ":") ~ (term <~ ")") ~ ("*" ~> term) ^^ {
-      case x ~ a ~ b => Sigma(a, Lam(x, a, b))
+    rep1(binder) ~ ("*" ~> term) ^^ {
+      case binders ~ body =>
+        binders.foldRight(body) { case ((xs, ty), acc) =>
+          xs.foldRight(acc)((x, b) => Sigma(ty, Lam(x, ty, b)))
+        }
     }
   
   def pathType: Parser[Term] =
@@ -47,20 +68,50 @@ object Parser extends RegexParsers:
       case a ~ u ~ v => PathP(a, u, v)
     }
   
+  // Path abstraction: <i> expr
+  def pathAbs: Parser[Term] =
+    ("<" ~> rep1(ident) <~ ">") ~ term ^^ {
+      case is ~ body => is.foldRight(body)((i, b) => Lam(i, Univ, b))
+    }
+  
   def lamExpr: Parser[Term] =
     "\\" ~> rep1(ident) ~ ("->" ~> term) ^^ {
       case xs ~ body => xs.foldRight(body)((x, b) => Lam(x, Univ, b))
     }
   
-  def app: Parser[Term] = 
-    atom ~ rep(atom) ^^ {
-      case f ~ args => args.foldLeft(f)(App.apply)
+  // Split expression: split { pat -> expr ; ... } or split\n pat -> expr\n ...
+  def splitExpr: Parser[Term] =
+    "split" ~> ("{" ~> rep1sep(splitCase, opt(";")) <~ "}") ^^ { cases =>
+      // For now, represent as a lambda (simplified)
+      Lam("x", Univ, cases.head._2)
+    } |
+    "split" ~> rep1(splitCase) ^^ { cases =>
+      Lam("x", Univ, cases.head._2)
     }
+  
+  def splitCase: Parser[(String, Term)] =
+    opt("|") ~> ident ~ rep(ident) ~ ("->" ~> term) ^^ {
+      case pat ~ args ~ body => (pat, body)
+    }
+  
+  def app: Parser[Term] = 
+    atom ~ rep(atom | pathApp) ^^ {
+      case f ~ args => args.foldLeft(f) {
+        case (fn, arg) => App(fn, arg)
+      }
+    }
+  
+  // Path application: @i
+  def pathApp: Parser[Term] =
+    "@" ~> ident ^^ Var.apply
   
   def atom: Parser[Term] =
     "U" ^^^ Univ |
     ident ^^ Var.apply |
-    "(" ~> term <~ ")"
+    "(" ~> repsep(term, ",") <~ ")" ^^ {
+      case List(t) => t  // Single element: just the term
+      case ts => ts.reduceRight((a, b) => App(App(Var("pair"), a), b))  // Tuple
+    }
   
   // ── Declarations ─────────────────────────────────────────
   
@@ -71,18 +122,32 @@ object Parser extends RegexParsers:
     "import" ~> ident ^^^ ()
   
   def decl: Parser[Decl] =
-    ("data" ~> ident) ~ ("=" ~> rep1(constructor)) ^^ {
+    ("data" ~> ident) ~ ("=" ~> rep1sep(constructor, "|")) ^^ {
       case name ~ constrs => DataDecl(name, Nil, Univ, constrs)
     } |
-    (ident <~ ":") ~ (term <~ "=") ~ term ^^ {
-      case name ~ ty ~ body => DefDecl(name, ty, body)
+    ident ~ rep(binder) ~ (":" ~> term) ~ opt("=" ~> term ~ opt(whereClause)) ^^ {
+      case name ~ params ~ ty ~ bodyOpt =>
+        val fullType = params.foldRight(ty) { case ((xs, pty), acc) =>
+          xs.foldRight(acc)((x, b) => Pi(pty, Lam(x, pty, b)))
+        }
+        bodyOpt match {
+          case Some(body ~ _) => DefDecl(name, fullType, body)
+          case None => DefDecl(name, fullType, Var("undefined"))
+        }
     }
   
   def constructor: Parser[(String, Term)] =
-    opt("|") ~> ident ~ opt("(" ~> ident ~ (":" ~> term) <~ ")") ^^ {
-      case name ~ Some(param ~ ty) => (name, ty)
-      case name ~ None => (name, Univ)
+    ident ~ rep(binder) ^^ {
+      case name ~ Nil => (name, Univ)
+      case name ~ binders => 
+        val ty = binders.foldRight(Univ: Term) { case ((xs, a), acc) =>
+          xs.foldRight(acc)((_, b) => Pi(a, Lam("_", a, b)))
+        }
+        (name, ty)
     }
+  
+  def whereClause: Parser[List[Decl]] =
+    "where" ~> rep(decl)
   
   def module: Parser[List[Decl]] = 
     opt(moduleHeader) ~> rep(importDecl) ~> rep(decl)
