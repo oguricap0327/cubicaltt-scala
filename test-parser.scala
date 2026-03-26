b1da66f54ce0d74bbd392955fdbb42d04dec91eb
Oguri Cap <oguri.cap@hotmail.com>
2026-03-26 16:52:48 +0800

    Remove test-parser.scala from project root

--- test-parser.scala
+++
@@ -1,9 +0,0 @@
-import cubical.Parser
-
-@main def testParser() =
-  val input = "data bool = false | true"
-  println(s"Parsing: $input")
-  Parser.parseModule(input) match {
-    case Right(decls) => println(s"Success: $decls")
-    case Left(err) => println(s"Error: $err")
-  }
